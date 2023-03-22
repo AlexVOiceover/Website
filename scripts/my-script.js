4427ba5282cba2c75c54ea6b5d413e251b7b3244
Alexander Rodriguez <alexrguez@gmail.com>
2023-03-22 20:20:03 +0000

    Added a CSS grid

--- scripts/my-script.js
+++ scripts/my-script.js
@@ -1,3 +1,16 @@
 console.log("Initialising console");
 
 
+const titles = document.querySelectorAll('h2.collapsible');
+
+  // Loop through the title elements and add a click event listener to each
+  titles.forEach(title => {
+    title.addEventListener('click', () => {
+      // Get the corresponding flex container element
+      const container = title.nextElementSibling;
+      // Toggle the collapsed class on the container element
+      container.classList.toggle('collapsed');
+
+    });
+  });
+
